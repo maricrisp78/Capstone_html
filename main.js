912289dc3fd90707474ca88ef3cad1d41b22cabf
marieashley_78 <marieashley_78@hotmail.com>
2016-02-11 13:04:14 -0500

    commit

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-$(document).ready(function(){
-var header = $('body');
+$(document).ready (function() {
 
-var backgrounds = new Array(
-    'url(images/oldschoolvan.jpg)'
-  , 'url(images/palmtrees.jpg)'
-  , 'url(images/sunset - high res.jpg)'
-  , 'url(images/Burano-Italy.jpg)'
-);
-
-var current = 0;
-
-function nextBackground() {
-    current++;
-    current = current % backgrounds.length;
-    header.css('background-image', backgrounds[current]);
-}
-setInterval(nextBackground, 1000);
-
-header.css('background-image', backgrounds[0]);
+$(".button").click (function() {
+$.getJSON("http://api.flckr.com/services/feeds/photos_public.gne?jsoncallback=?",)
+      {
+              tags: "car",
+              tagmode: "any",
+              format: "json"
+          }, function (data) {
+      $.each(data.items, function(i, item){
+        $('<img/>').attr("src", item.media.m).appendTo('#images');
+        if(i==3) return false;
+           
+      });
+    });
+  
+  });
+                   
 });
+
+
+
+
+
+
+
+// $(document).ready(function(){
+// var header = $('body');
+
+// var backgrounds = new Array(
+//     'url(images/oldschoolvan.jpg)'
+//   , 'url(images/palmtrees.jpg)'
+//   , 'url(images/sunset - high res.jpg)'
+//   , 'url(images/Burano-Italy.jpg)'
+// );
+
+// var current = 0;
+
+// function nextBackground() {
+//     current++;
+//     current = current % backgrounds.length;
+//     header.css('background-image', backgrounds[current]);
+// }
+// setInterval(nextBackground, 1000);
+
+// header.css('background-image', backgrounds[0]);
+// });
